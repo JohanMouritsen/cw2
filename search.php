918bdf1415b2745cfff7fbe9ba5c2a173fbb4118
JohanMouritsen <nikrionikrio@live.dk>
2014-10-31 01:00:36 +0000

    commit 2

---
+++ search.php
@@ -0,0 +1,29 @@
+<?php 
+
+$search=$_POST['search']; 
+//connect  to the database 
+$db=mysql_connect  ("eu-cdbr-azure-west-b.cloudapp.net", "b0f27f5ecf18c9",  "d3b9c22a") or die ('I cannot connect to the database  because: ' . mysql_error()); 
+//-select  the database to use 
+$mydb=mysql_select_db("myWebJoABWSQBed1"); 
+//-query  the database table 
+$sql="SELECT * FROM registration_tbl 
+		WHERE name LIKE '%$search%' 
+		OR email LIKE '%$search%' 
+		OR company LIKE '%$search%'
+		OR date LIKE '%$search%' 
+		ORDER by name ASC";
+//-run  the query against the mysql query function 
+$result=mysql_query($sql);
+$number=mysql_num_rows($result);
+$pageTitle="Search Results";
+ 
+//-create  while loop and loop through result set 
+while($row=mysql_fetch_array($result)){ 
+        $name  =$row['name']; 
+        $email =$row['email']; 
+        $company =$row['company']; 
+        $date =$row['date']; }
+//-display the result of the array 
+print "
+<td><strong> $name $email $company $date </strong><vr />";
+?> 
